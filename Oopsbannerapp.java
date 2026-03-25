ad176a7a7dadf2dd97a45c98364ce086bc2fe9d7
jaswanthmarinedi-ai <jaswanthmarinedi@gmail.com>
2026-03-25 09:34:30 +0530

    feature uc1

---
+++ Oopsbannerapp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp - UC1
+ * Prints the text "OOPS" to the console
+ * 
+ * @author Jaswanth
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
